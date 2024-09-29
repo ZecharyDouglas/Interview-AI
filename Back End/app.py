62bc15b23fc16d233ec54ffde31f58c4fac4149a
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2024-09-29 17:46:30 -0400

    successful sign in backend

---
+++ Back End/app.py
@@ -0,0 +1,64 @@
+from flask import Flask , jsonify , request
+import boto3
+import botocore
+from botocore.exceptions import ClientError
+from flask_cors import CORS
+from datetime import datetime
+
+#creates the app and then enables the CORS for all domains
+app = Flask(__name__)
+CORS(app)
+
+#getting the dynamodb service resource
+session = boto3.Session(profile_name='default')
+dynamodb = session.resource('dynamodb', region_name='us-east-1')
+users = dynamodb.Table('Users')
+
+#test for listening
+@app.route('/' , methods=['GET'])
+def method_name():
+    return jsonify({
+        'message':'The API works!'
+    })
+    
+
+#post request to sign up user
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    name = data.get('name')
+    user_id = data.get('user_id')
+    entry_time = datetime.now().isoformat()
+    try:
+        users.put_item(
+            Item={
+                'user_id': user_id,
+                "entry_time":entry_time,
+                'username': username,
+                'password': password,
+                'name':name,
+                
+            }
+        )
+        return jsonify({
+            'message':"User was created successfully"
+        })
+    except Exception as e:
+        return jsonify({
+            "error":str(e) 
+        }) , 500
+
+    
+#post request to sign in a user
+#middleware?
+
+#get request to get a user's confidence metrics
+#get request to see if a user is signed in 
+#get request to plot user progress in charts
+
+#post request to call model processing and return results to the database
+
+if __name__ == '__main__':
+    app.run(debug=True)
