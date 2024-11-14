a95bde44d057f95660d1956cc573b987f7c8fe3e
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2024-11-13 22:06:12 -0500

    no more mui

--- Front End/src/Insights.tsx
+++ Front End/src/Insights.tsx
@@ -70,64 +70,41 @@ export default function Insights() {
     <div className="flex flex-col items-center ml-10 mr-10 mt-10">
       <div className=" flex-col align-top mt-0">
         <div className=" mb-10">
-          <h1 className=" text-3xl">Insights Dashboard</h1>
+          <h1 className="  font-Inter text-3xl font-thin">
+            Insights Dashboard
+          </h1>
         </div>
-        <div className=" flex flex-col">
-          <Grid
-            container
-            spacing={{ xs: 2, md: 3 }}
-            columns={{ xs: 4, sm: 8, md: 10 }}
-          >
-            {confidenceValues.map((value, index) => (
-              <Grid item xs={1} sm={1} md={2} key={index}>
-                <CardContent className=" bg-customlighterblue shadow-md">
-                  <Typography
-                    gutterBottom
-                    sx={{
-                      color: "text.secondary",
-                      fontSize: 14,
-                    }}
-                  >
-                    {value.topic}
-                  </Typography>
-                  <Typography variant="h5" component="div"></Typography>
-                  <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
-                    adjective
-                  </Typography>
-                  <Typography className=" bg-white">
-                    <RadialBarChart
-                      width={100}
-                      height={100}
-                      innerRadius="60%"
-                      outerRadius="100%"
-                      data={[{ value: value.confidence, name: value.topic }]}
-                      startAngle={180}
-                      endAngle={0}
-                      className=" ml-20"
-                    >
-                      <RadialBar
-                        minAngle={15}
-                        label={{ fill: "#CCF", position: "insideCenter" }}
-                        background
-                        clockWise={true}
-                        dataKey="value"
-                      />
-                      <Tooltip />
-                    </RadialBarChart>
-                  </Typography>
-                  <CardActions>
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </CardContent>
-              </Grid>
-            ))}
-          </Grid>
+        <div className="grid grid-cols-5 grid-rows-2 ">
+          {confidenceValues.map((value, index) => (
+            <div className=" bg-slate-50 shadow-md rounded-md m-4 p-10 border-2 border-black flex items-center justify-center">
+              <RadialBarChart
+                width={100}
+                height={100}
+                innerRadius="60%"
+                outerRadius="100%"
+                data={[{ value: value.confidence, name: value.topic }]}
+                startAngle={180}
+                endAngle={0}
+                className=""
+              >
+                <RadialBar
+                  minAngle={15}
+                  label={{ fill: "#CCF", position: "insideCenter" }}
+                  background
+                  clockWise={true}
+                  dataKey="value"
+                  className=""
+                />
+                <Tooltip />
+              </RadialBarChart>
+            </div>
+          ))}
         </div>
         <div className=" mt-10">
-          <h1 className=" text-2xl">Top 5 Tips</h1>
+          <h1 className=" text-2xl font-thin">Top 5 Tips</h1>
           <ul>
             {[...Array(5)].map((_, index) => (
-              <li className="mt-5" key={index}>
+              <li className="mt-5 font-Inter font-thin" key={index}>
                 {" "}
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero
                 alias vero qui. Facere sint, veniam eius recusandae,
